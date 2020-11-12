742ccdd6095d0f5fe6415cb33e3a2c50154c50e8
Himanshu Rawat <himanshu.rawat@nih.gov>
2020-11-12 15:18:51 -0500

    Changes by 'Himanshu Rawat' on 'esr - 1.0.9' -- Build #1

---
+++ sn/esr/Dictionary Entry/x_g_nci_esr_ticket/active_true/esr_type/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : esr
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-04-09 19:17:20
+ * Created By  : saxenap2@nih.gov
+ * Updated On  : 2020-08-13 21:35:39
+ * Updated By  : rawath2@nih.gov
+ * URL         : /sys_dictionary.do?sys_id=3c00e9c21b8098902701b913cc4bcb69
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
