4626b58fed648cc7eca21c840306f8090b972a57
Rizk <rizknt@nih.gov>
2020-11-12 15:21:01 -0500

    Changes by 'undefined' on 'esr - 1.0.9' -- Build #1

---
+++ sn/esr/Dictionary Entry/x_g_nci_esr_ticket/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : esr
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-04-01 17:10:39
+ * Created By  : saxenap2@nih.gov
+ * Updated On  : 2020-10-30 17:49:52
+ * Updated By  : system
+ * URL         : /sys_dictionary.do?sys_id=4966b35f1bb7c8502701b913cc4bcb1d
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
